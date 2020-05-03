e7f895e404361f2b9a3594d229af7f16124e6d03
Deku <dekutran@gmail.com>
2020-05-02 21:11:01 -0700

    temporarily add all untracked file

--- src/components/LeafletMap.js
+++ src/components/LeafletMap.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
 import 'leaflet/dist/leaflet.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 class LeafletMap extends Component {
@@ -16,8 +17,8 @@ class LeafletMap extends Component {
 	render() {
 		const position = [this.state.lat, this.state.lng];
 		return (
-			<div>
-				<Map center={position} zoom={this.state.zoom} style={{height : '98vh', zIndex: 0}}>
+			<div className='pt-5'>
+				<Map center={position} zoom={this.state.zoom} style={{height : '95vh', zIndex: 0}}>
 					<TileLayer
 						attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
 						url="https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png"
